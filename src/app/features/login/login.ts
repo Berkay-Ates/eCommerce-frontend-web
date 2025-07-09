80b5a2a722e4986b1d12e790db8072adcf7fb301
Berkay Ates <atesberkay2356@gmail.com>
2025-07-09 11:33:28 +0300

    feat: refactor login method to use async/await and improve error handling

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Auth } from '../../core/services/auth';
 import { FormsModule } from '@angular/forms';
 import { UserType } from '../../static/enums/user_types';
+import { LoginEntityRequest } from '../../core/models/Auth/requestModels/requestLoginObject';
 
 @Component({
   selector: 'app-login',
@@ -9,32 +10,40 @@ import { UserType } from '../../static/enums/user_types';
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
+
 export class Login {
   authService = inject(Auth)
   email: string = '';
   password: string = '';
   userType: UserType = UserType.CUSTOMER;
 
-  login() {
+  async login() {
     if (!this.email || !this.password) {
       console.log('Email ve şifre zorunlu');
       return;
     }
-    console.log("Clicked");
 
-    
-    
+    let loginObject: LoginEntityRequest = {email:this.email, password: this.password}
+    try {
 
-    // this.authService.login(this.email, this.password).subscribe({
-    //   next: (res:any) => {
-    //     console.log('Giriş başarılı', res);
-    //     // buraya yönlendirme vs. ekleyebilirsin
-    //   },
-    //   error: (err:any) => {
-    //     console.error('Giriş başarısız', err);
-    //   }
-    // });
+      if(this.userType===UserType.CUSTOMER){
+        console.log("Customer Login")
+        console.log(loginObject);
+        
+        const res = await this.authService.loginCustomer(loginObject);
+        console.log('Giriş başarili', res);
+
+      }else if(this.userType === UserType.SELLER){
+        console.log("Seller Login")
+        const res = await this.authService.loginSeller(loginObject);
+        console.log('Giriş başarili', res);
+      }
+
+    } catch (err: any) {
+      console.error('Giriş başarisiz', err);
+      console.log(err)
+      // hata mesaji göster
+    }
   }
-
 
 }
